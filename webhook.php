bcd9028da36880c6671de0f2a680e6657590c0b2
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2018-09-20 09:15:46 -0400

    t

--- webhook.php
+++ webhook.php
@@ -15,7 +15,7 @@
     // error_log(print_r($input['entry'][0]['changes'][0]['value']->leadgen_id, true));
     // error_log(print_r('------------------------------------', true));
     $leadgen_id = $input['entry'][0]['changes'][0]['value']['leadgen_id'];
-    error_log(print_r($leadgen_id));
+    error_log(print_r($leadgen_id, true));
     // error_log(print_r($input['leadgen']->leadgen_id, true));
     // error_log(print_r('-------------------------------', true));
     // error_log(print_r($input[0], true));
