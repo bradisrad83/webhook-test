6e19c20c141574b34d7d0ffabeaa62bb032cdda7
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2018-09-20 08:37:17 -0400

    maybe maybe

--- webhook.php
+++ webhook.php
@@ -8,9 +8,9 @@
     }
 
     $input = json_decode(file_get_contents('php://input'), true);
-    error_log(var_dump($input));
-    // error_log(print_r('-------------------------------', true));
-    // error_log(print_r($input[0], true));
+    error_log(print_r($input, true));
+    error_log(print_r('-------------------------------', true));
+    error_log(print_r($input->value->leadgen_id, true));
     // error_log(print_r('-------------------------------', true));
     // error_log(print_r($input[0]->leadgen_id, true));
     // error_log(print_r('-------------------------------', true));
