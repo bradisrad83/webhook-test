f95d9234fc92160e86eddb56e326c661d9821ed6
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2018-09-20 08:18:32 -0400

    changes again

--- webhook.php
+++ webhook.php
@@ -9,7 +9,13 @@
 
     $input = json_decode(file_get_contents('php://input'), true);
     error_log(print_r($input, true));
-    // print_r($input, true);
+    error_log(print_r('-------------------------------', true));
+    error_log(print_r($input[0], true));
+    error_log(print_r('-------------------------------', true));
+    error_log(print_r($input[0]->leadgen_id, true));
+    error_log(print_r('-------------------------------', true));
+    error_log(print_r($input[0]['leadgen_id'], true));
+    error_log(print_r('-------------------------------', true));
 
 
 
