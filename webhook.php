4d7b6386d24615dbcf4aee8da660c1ac42142cff
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2018-09-24 16:25:23 -0400

    tseting some more

--- webhook.php
+++ webhook.php
@@ -1,11 +1,11 @@
 <?php
 
-    // $challenge = $_REQUEST['hub_challenge'];
-    // $verify_token = $_REQUEST['hub_verify_token'];
+    $challenge = $_REQUEST['hub_challenge'];
+    $verify_token = $_REQUEST['hub_verify_token'];
 
-    // if ($verify_token === 'abcd1234') {
-    //     echo $challenge;
-    // }
+    if ($verify_token === 'abcd1234') {
+        echo $challenge;
+    }
 
     // $input = json_decode(file_get_contents('php://input'), true);
     // error_log(print_r($input, true));
