9e9cd1092d0112b49bcc7e65dcc996a1dc0ac4c1
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2018-09-24 16:28:13 -0400

    fkdjf

--- webhook.php
+++ webhook.php
@@ -4,7 +4,8 @@
     $verify_token = $_REQUEST['hub_verify_token'];
 
     if ($verify_token === 'abcd1234') {
-        echo $challenge;
+        // echo $challenge;
+        echo '1445840444';
     }
 
     // $input = json_decode(file_get_contents('php://input'), true);
