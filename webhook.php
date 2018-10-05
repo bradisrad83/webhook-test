7bf6b39aab169aa87465d7d5105655a3b2ffecc6
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2018-10-05 08:31:11 -0400

    testing

--- webhook.php
+++ webhook.php
@@ -17,19 +17,40 @@
 
      error_log(print_r($accessToken, true));
      error_log(print_r('---------------------------------------------------------------------', true));
+     error_log(print_r($accessToken->access_token, true));
+     error_log(print_r('[][][][][][[][][][][][][][][][][][][][][][][][][][][][][][][][][][][]', true));
+     error_log(print_r($accessToken->token_type, true));
+     error_log(print_r('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$', true));
 
 
 
-    //CURL CALL TO GET ACCESS TOKEN
-    $ch2 = curl_init();
-    curl_setopt($ch2, CURLOPT_URL,            "https://graph.facebook.com/563763967129243?key=value&access_token=268096574032221|273603e7fba3283b9d8b2a58ebb6f77a");
-    curl_setopt($ch2, CURLOPT_RETURNTRANSFER,    1 );
-    curl_setopt($ch2, CURLOPT_HTTPGET,           1 );
-    curl_setopt($ch2, CURLOPT_HTTPHEADER,     array('Content-Type: text/plain')); 
-     $test=curl_exec($ch2);
 
-     error_log(print_r($test, true));
-     error_log(print_r('{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}', true));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // //CURL CALL TO GET ACCESS TOKEN
+    // $ch2 = curl_init();
+    // curl_setopt($ch2, CURLOPT_URL,            "https://graph.facebook.com/563763967129243?key=value&access_token=268096574032221|273603e7fba3283b9d8b2a58ebb6f77a");
+    // curl_setopt($ch2, CURLOPT_RETURNTRANSFER,    1 );
+    // curl_setopt($ch2, CURLOPT_HTTPGET,           1 );
+    // curl_setopt($ch2, CURLOPT_HTTPHEADER,     array('Content-Type: text/plain')); 
+    //  $test=curl_exec($ch2);
+
+    //  error_log(print_r($test, true));
+    //  error_log(print_r('{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}', true));
 
     // //CURL CALL TO GET PAGE ACCESS TOKEN USING PREVIOUS ACCESS TOKEN
     // $pageId = '563763967129243';
