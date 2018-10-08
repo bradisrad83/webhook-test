87461404af8f44b318d08b6149286cbf74931ea6
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2018-10-08 11:48:19 -0400

    this one

--- webhook.php
+++ webhook.php
@@ -59,7 +59,7 @@
         'notes'         => $notes
     ]);
     $headerForAcculynx = [
-        'Authorization' => 'Bearer N2QyMDFjZGYtNmE5ZS00MDE5LWFjNTgtZWQ5ODljZTU3Y2E1ODJiMzQzMzctZDQ0ZC00MTZkLWI5MDAtNjVlNDZlN2U1MDRh',
+        'Authorization' => 'Bearer Y2FkNzQ0ZGEtMjBmOC00YzJkLWExMzMtOGU5YTkxNGFhNTZmMGZkYzUyOTYtZWI5Zi00NDk4LWJkYWQtZDJmN2Q4MzEzZjU4',
         'Content-Type'  => 'application/json'
     ];
 
@@ -78,7 +78,10 @@
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
     curl_setopt($ch, CURLOPT_POST,           1 );
     curl_setopt($ch, CURLOPT_POSTFIELDS,     $jsonArrayForAcculynx); 
-    curl_setopt($ch, CURLOPT_HTTPHEADER,     $headerForAcculynx);  
+    curl_setopt($ch, CURLOPT_HTTPHEADER,     array(
+                                        'Authorization' => 'Bearer Y2FkNzQ0ZGEtMjBmOC00YzJkLWExMzMtOGU5YTkxNGFhNTZmMGZkYzUyOTYtZWI5Zi00NDk4LWJkYWQtZDJmN2Q4MzEzZjU4',
+                                        'Content-Type'  => 'application/json'
+    ));  
     $results=curl_exec($ch);
     error_log(print_r('Below should be a response of 200 and Ill have to check acculynx to see if it went through', true));
     error_log(print_r($results, true));
