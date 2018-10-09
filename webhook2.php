a74350bb3947ff5355e12773390e17a8f3c0c55a
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2018-10-09 14:55:14 -0400

    lslsls'

--- webhook2.php
+++ webhook2.php
@@ -1,22 +1,15 @@
 <?php
-    //  CURL CALL TO ACCULLYNX
-    // $ch = curl_init();
 
-    // curl_setopt($ch, CURLOPT_URL,            "https://api.acculynx.com/api/v1/leads");
-    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
-    // curl_setopt($ch, CURLOPT_POST,           1 );
-    // curl_setopt($ch, CURLOPT_POSTFIELDS,     "body goes here" ); 
-    // curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Authorization': 'Bearer N2QyMDFjZGYtNmE5ZS00MDE5LWFjNTgtZWQ5ODljZTU3Y2E1ODJiMzQzMzctZDQ0ZC00MTZkLWI5MDAtNjVlNDZlN2U1MDRh', 'Content-Type: application/json'));  
-    // $result=curl_exec($ch);
-    // curl_close($ch);
-  
-   
-   
-    //  most likeley the body of the form
-    // {
-    //     'firstName': 'form_full_name',
-    //     'emailAddress': 'form_email_address',
-    //     'phoneNumber1': 'form_phone_number',
-    //     'street': 'form_address',
-    //     'notes': 'service_inquiry_form',
-    // }
+$challenge = $_REQUEST['hub_challenge'];
+$verify_token = $_REQUEST['hub_verify_token'];
+
+if ($verify_token === 'abcd1234') {
+    echo $challenge;
+}
+
+$input = json_decode(file_get_contents('php://input'), true);
+error_log(print_r($input, true));
+
+
+
+?>
