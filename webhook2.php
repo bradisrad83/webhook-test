cabe4077fb4b9e5e05f71837e151840c312f2db1
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2018-10-18 10:26:19 -0400

    final test

--- webhook2.php
+++ webhook2.php
@@ -14,15 +14,47 @@ error_log(print_r($input, true));
 error_log(print_r('the above code is proof of concept that we will be linking Squiblib (our APP) to this webook.', true));
 
 
-$pageName = $input['entry'][0]['changes'][0]['value']['from']['name'];
-error_log(print_r($pageName, true));
-$pageId = $input['entry'][0]['changes'][0]['value']['from']['id'];
-error_log(print_r($pageId, true));
-$postId = $input['entry'][0]['changes'][0]['value']['post_id'];
-error_log(print_r($postId, true));
-$message = $input['entry'][0]['changes'][0]['value']['message'];
-error_log(print_r($message, true));
+// $pageName = $input['entry'][0]['changes'][0]['value']['from']['name'];
+// error_log(print_r($pageName, true));
+// $pageId = $input['entry'][0]['changes'][0]['value']['from']['id'];
+// error_log(print_r($pageId, true));
+// $postId = $input['entry'][0]['changes'][0]['value']['post_id'];
+// error_log(print_r($postId, true));
+// $message = $input['entry'][0]['changes'][0]['value']['message'];
+// error_log(print_r($message, true));
+$status = $input['entry'][0]['changes'][0]['value']['item'];
+error_log(print_r($status, true));
+// $fields = $input['entry'][0]['changes'][0]['value'];
+error_log(print_r($fields, true));
 
+
+if($status = 'status') {
+    // $fields = array ([
+    //     'page_id'       => $input['entry'][0]['changes'][0]['value']['from']['id'],
+    //     'page_name'     => $input['entry'][0]['changes'][0]['value']['from']['name'],
+    //     'post_id'       => $input['entry'][0]['changes'][0]['value']['post_id'],
+    //     'message'       => $input['entry'][0]['changes'][0]['value']['message'],
+    // ]);
+    //  CURL CALL TO SQUIBLIB TO ALLOW FOR BOOSTABLE POST TO BECOME A NEW AD
+    $squibCurl = curl_init();
+
+    curl_setopt($squibCurl, CURLOPT_URL,            "https://suiblib.dev/boostpost");
+    curl_setopt($squibCurl, CURLOPT_RETURNTRANSFER,                                      1 );
+    curl_setopt($squibCurl, CURLOPT_POST,                                                1 );
+    // curl_setopt($squibCurl, CURLOPT_POSTFIELDS,                                     $fields); 
+    curl_setopt($squibCurl, CURLOPT_POSTFIELDS,                                                     array ([
+                    'page_id'       => $input['entry'][0]['changes'][0]['value']['from']['id'],
+                    'page_name'     => $input['entry'][0]['changes'][0]['value']['from']['name'],
+                    'post_id'       => $input['entry'][0]['changes'][0]['value']['post_id'],
+                    'message'       => $input['entry'][0]['changes'][0]['value']['message'],
+                                                                                                        ]) ); 
+    // curl_setopt($squibCurl, CURLOPT_HTTPHEADER, array('Authorization: Bearer Y2FkNzQ0ZGEtMjBmOC00YzJkLWExMzMtOGU5YTkxNGFhNTZmMGZkYzUyOTYtZWI5Zi00NDk4LWJkYWQtZDJmN2Q4MzEzZjU4', 'Content-Type: application/json'));
+    curl_setopt($squibCurl, CURLOPT_HTTPHEADER,     array('Content-Type: application/json'));
+    $results = curl_exec($squibCurl);
+    error_log(print_r('--------------------------------------------------------------------------------------------------------------------------------------------------', true));
+    error_log(print_r($results, true));
+
+}
 
 
 
