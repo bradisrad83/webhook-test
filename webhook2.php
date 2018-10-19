fc04d3c1d6d605bd6bdb340aae8e300f3e268b1e
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2018-10-19 10:31:30 -0400

    testing

--- webhook2.php
+++ webhook2.php
@@ -41,7 +41,7 @@ if($status = 'status') {
         'page_name'     => $input['entry'][0]['changes'][0]['value']['from']['name'],
         'post_id'       => $input['entry'][0]['changes'][0]['value']['post_id'],
         'message'       => $input['entry'][0]['changes'][0]['value']['message'],
-        'access_token'  => 'EAADz1RkPiV0BACHPqZBUrGdqWZAap24s2ovbNMPelxoOKCzQY0s6dxZB6TMZCb9TBSZBT2giGW7UJzpk19tnx1leeZB00BnO6G4KWBAtTxDWdZAEJHigrQvXvycD12lI7hvpnZA3Rr1qRSX4jVajRdj5hyxzvUxjSjlFZBTLaE2Jr8gZDZD'
+        'state'         => 'EAADz1RkPiV0BACHPqZBUrGdqWZAap24s2ovbNMPelxoOKCzQY0s6dxZB6TMZCb9TBSZBT2giGW7UJzpk19tnx1leeZB00BnO6G4KWBAtTxDWdZAEJHigrQvXvycD12lI7hvpnZA3Rr1qRSX4jVajRdj5hyxzvUxjSjlFZBTLaE2Jr8gZDZD'
     ]));
     curl_setopt($squibCurl, CURLOPT_HTTPHEADER,     array('Content-Type: application/json'));
     $results = curl_exec($squibCurl);
