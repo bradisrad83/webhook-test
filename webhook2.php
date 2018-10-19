6f4c2a2b556db75775801be88117856904b8e860
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2018-10-19 13:00:34 -0400

    testing

--- webhook2.php
+++ webhook2.php
@@ -27,26 +27,24 @@ error_log(print_r($status, true));
 
 
 if($status == 'status') {
-    error_log(print_r('this should hopefully appear on the screen', true));
+    //  CURL CALL TO SQUIBLIB TO ALLOW FOR BOOSTABLE POST TO BECOME A NEW AD
+    $squibCurl = curl_init();
+
+    // curl_setopt($squibCurl, CURLOPT_URL,            "https://squiblib.dev/boostpost");
+    curl_setopt($squibCurl, CURLOPT_URL,               "https://7cb370fd.ngrok.io /boostpost");
+    curl_setopt($squibCurl, CURLOPT_RETURNTRANSFER,                                      1 );
+    curl_setopt($squibCurl, CURLOPT_POST,                                                1 );
+    curl_setopt($squibCurl, CURLOPT_POSTFIELDS,                                     json_encode([
+        'page_id'       => $input['entry'][0]['changes'][0]['value']['from']['id'],
+        'page_name'     => $input['entry'][0]['changes'][0]['value']['from']['name'],
+        'post_id'       => $input['entry'][0]['changes'][0]['value']['post_id'],
+        'message'       => $input['entry'][0]['changes'][0]['value']['message'],
+    ]));
+    curl_setopt($squibCurl, CURLOPT_HTTPHEADER,     array('Content-Type: application/json'));
+    $results = curl_exec($squibCurl);
+    error_log(print_r('--------------------------------------------------------------------------------------------------------------------------------------------------', true));
+    error_log(print_r($results, true));
+
 }
-//     //  CURL CALL TO SQUIBLIB TO ALLOW FOR BOOSTABLE POST TO BECOME A NEW AD
-//     $squibCurl = curl_init();
-
-//     // curl_setopt($squibCurl, CURLOPT_URL,            "https://squiblib.dev/boostpost");
-//     curl_setopt($squibCurl, CURLOPT_URL,               "https://7cb370fd.ngrok.io /boostpost");
-//     curl_setopt($squibCurl, CURLOPT_RETURNTRANSFER,                                      1 );
-//     curl_setopt($squibCurl, CURLOPT_POST,                                                1 );
-//     curl_setopt($squibCurl, CURLOPT_POSTFIELDS,                                     json_encode([
-//         'page_id'       => $input['entry'][0]['changes'][0]['value']['from']['id'],
-//         'page_name'     => $input['entry'][0]['changes'][0]['value']['from']['name'],
-//         'post_id'       => $input['entry'][0]['changes'][0]['value']['post_id'],
-//         'message'       => $input['entry'][0]['changes'][0]['value']['message'],
-//     ]));
-//     curl_setopt($squibCurl, CURLOPT_HTTPHEADER,     array('Content-Type: application/json'));
-//     $results = curl_exec($squibCurl);
-//     error_log(print_r('--------------------------------------------------------------------------------------------------------------------------------------------------', true));
-//     error_log(print_r($results, true));
-
-// }
 //TOKEN THAT NEVER EXPIRES:    EAAQbS2lfKcQBAE3kiGVZAn7NNmMS4n64XvaKrzO32295jBUapMlMFI9FYYdJTn3g9KiyVJK95fN7eIk2BGIKEl2nsG45MX4CKw2fdA5z9JmPyxmJY1YUZBoYhVaS5kxUfjYPmfRdc8HE6BbS74p2f4ZAjDooGgZD
 ?>
