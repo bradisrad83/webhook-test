218c09832a8c6bb7b3388d877185beaa53d8793f
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2018-10-19 09:41:26 -0400

    test

--- webhook2.php
+++ webhook2.php
@@ -39,16 +39,20 @@ if($status = 'status') {
     $squibCurl = curl_init();
 
     // curl_setopt($squibCurl, CURLOPT_URL,            "https://squiblib.dev/boostpost");
+
+
+
+    
     curl_setopt($squibCurl, CURLOPT_URL,               "https://96f6001e.ngrok.io/boostpost");
     curl_setopt($squibCurl, CURLOPT_RETURNTRANSFER,                                      1 );
     curl_setopt($squibCurl, CURLOPT_POST,                                                1 );
-    curl_setopt($squibCurl, CURLOPT_POSTFIELDS,                                                     array ([
+    curl_setopt($squibCurl, CURLOPT_POSTFIELDS,                                                     json_encode(array ([
                     'page_id'       => $input['entry'][0]['changes'][0]['value']['from']['id'],
                     'page_name'     => $input['entry'][0]['changes'][0]['value']['from']['name'],
                     'post_id'       => $input['entry'][0]['changes'][0]['value']['post_id'],
                     'message'       => $input['entry'][0]['changes'][0]['value']['message'],
-                                                                                                        ]) ); 
-    // curl_setopt($squibCurl, CURLOPT_HTTPHEADER,     array('Content-Type: application/json'));
+                                                                                                        ]), true )); 
+    curl_setopt($squibCurl, CURLOPT_HTTPHEADER,     array('Content-Type: application/json'));
     $results = curl_exec($squibCurl);
     error_log(print_r('--------------------------------------------------------------------------------------------------------------------------------------------------', true));
     error_log(print_r($results, true));
