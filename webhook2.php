8799b24a8bf4d84201ea67f81ecafa8dbab74f9d
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2018-10-19 13:07:25 -0400

    more debugging

--- webhook2.php
+++ webhook2.php
@@ -31,14 +31,14 @@ if($status == 'status') {
     $squibCurl = curl_init();
 
     // curl_setopt($squibCurl, CURLOPT_URL,            "https://squiblib.dev/boostpost");
-    curl_setopt($squibCurl, CURLOPT_URL,               "https://7cb370fd.ngrok.io /boostpost");
+    curl_setopt($squibCurl, CURLOPT_URL,               "https://81d7e62d.ngrok.io/boostpost");
     curl_setopt($squibCurl, CURLOPT_RETURNTRANSFER,                                      1 );
     curl_setopt($squibCurl, CURLOPT_POST,                                                1 );
     curl_setopt($squibCurl, CURLOPT_POSTFIELDS,                                     json_encode([
         'page_id'       => $input['entry'][0]['changes'][0]['value']['from']['id'],
         'page_name'     => $input['entry'][0]['changes'][0]['value']['from']['name'],
         'post_id'       => $input['entry'][0]['changes'][0]['value']['post_id'],
-        'message'       => $input['entry'][0]['changes'][0]['value']['message'],
+        'message'       => 'AD: '.$input['entry'][0]['changes'][0]['value']['message'],
     ]));
     curl_setopt($squibCurl, CURLOPT_HTTPHEADER,     array('Content-Type: application/json'));
     $results = curl_exec($squibCurl);
