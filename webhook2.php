8af011e1593c552da4c9e03142bc7f897eb9e5d4
Brad Goldsmith <brad.m.goldsmith@gmail.com>
2018-11-05 08:05:39 -0500

    changed token on webhook2 so that facebook api can be tested through squiblib.

--- webhook2.php
+++ webhook2.php
@@ -48,5 +48,5 @@ if($status == 'status' || $status == 'photo' || $status == 'video' || $status ==
     error_log(print_r($results, true));
 
 }
-//TOKEN THAT NEVER EXPIRES:    EAAQbS2lfKcQBAJalq4xIUzka2tpPeX39unVMTEFdLEw2LqErUEVHYwyYJZCCJlC9XisJY0CMS9wBtU36jSc724CPXRF2yZBZB1WIQHlKyqkAQcTtCWSnuUM44YxuOqUgbmDUwbOe4HVlqR0smJg7tmDlVcFJhAZD
+//TOKEN THAT NEVER EXPIRES:    EAAQbS2lfKcQBAPCxFxaiMIJiIu4z9K4wcTJv5sA0MZC3QDrPXtuT0JJRaF65FTcVig7uH5Olt6BHsiutHOlJa8HZB5PhEngU566YjJCmwcEmyZB9ZCKGUW7hhtSC6DOgmMXLQ3LzH2pdwWyfqUE9nze52K6tVMUZD
 ?>
